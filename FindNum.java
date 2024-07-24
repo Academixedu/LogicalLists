d24c6f706b30537e3c7cd9add1de30e9aae91d30
MekaManojKrishnalakshman <mekamanojkrishnalakshman@gmail.com>
2024-07-24 18:38:40 +0530

    logicswrittten

--- FindNum.java
+++ FindNum.java
@@ -1,10 +1,23 @@
-public class FindNum{
-// Write a method to find whether a particular element is present in an ArrayList
-public static void main(String[]args){
-// Create an Object for ArrayList
-  // Take a For Loop and Load Numbers in to ArrayList using add()
-  // Contains is the Method name which you will create
-System.out.println("ArrayList contains 5: " + contains(arrayList, 5));
-  // it should return either true or false
-}
+import java.util.ArrayList;
+
+public class FindNum {
+    public static boolean contains(ArrayList<Integer> list, int num) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) == num) {
+                return true;  
+            }
+        }
+        return false; 
+    }
+
+
+    public static void main(String[] args) {
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        for (int i = 1; i <= 10; i++) {
+            a.add(i);
+        }
+        System.out.println(a);
+
+        System.out.println("ArrayList contains 5: " + contains(a, 5));
+    }
 }
--- Sum.java
+++ Sum.java
@@ -1,8 +1,25 @@
+import java.util.ArrayList;
 public class Sum{
+
+  public static int total(ArrayList<Integer> list) {
+    int sum =0;
+    for (int i = 0; i < list.size(); i++) {
+        sum += list.get(i);
+    }
+    return sum;
+  }
   // prepare a function to add and return all elements in ArrayList
   public static void main(String[]args){
-  // Create ArrayList Object
-    // Add Elements to it using For Loop
-     System.out.println("Sum of ArrayList elements: " + total(arrayList));
-  // total is your function name it should return total value
-  }}
+    ArrayList<Integer> ArrayList = new ArrayList<Integer>();
+
+    for (int i = 1; i <= 10; i++) {
+        ArrayList.add(i);
+    }
+    System.out.println(ArrayList);
+    
+  System.out.println("Sum of ArrayList elements: " + total(ArrayList));
+
+
+
+  }
+}
