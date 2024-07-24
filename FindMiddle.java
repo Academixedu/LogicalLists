7fd839f6216db5cff76b47fe69536eb05d9580dd
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-24 12:38:31 +0530

    Create FindMiddle.java

---
+++ FindMiddle.java
@@ -0,0 +1,13 @@
+public class FindMiddle{
+    public static <T> T Middle(ArrayList<T> list) {
+        return list.get(list.size() / 2);
+    }
+// prepare a function to return middle value of LinkedList
+  public static void main(String[]args){
+// create a LinkedList and ArrayList Objects
+    // prepare a for Loop of 1 to 10
+    // and add elements of for loop in both Objects using add functions
+        System.out.println("Middle of ArrayList: " + Middle(arrayList));
+    // write a print statement to print middle no of Linkedlist Object
+}
+}
