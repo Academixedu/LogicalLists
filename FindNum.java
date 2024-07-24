0d7c8c2cc85d697d0ed4003745be4bf701b22a18
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-24 12:43:49 +0530

    Create FindNum.java

---
+++ FindNum.java
@@ -0,0 +1,10 @@
+public class FindNum{
+// Write a method to find whether a particular element is present in an ArrayList
+public static void main(String[]args){
+// Create an Object for ArrayList
+  // Take a For Loop and Load Numbers in to ArrayList using add()
+  // Contains is the Method name which you will create
+System.out.println("ArrayList contains 5: " + contains(arrayList, 5));
+  // it should return either true or false
+}
+}
