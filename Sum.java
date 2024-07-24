bf19cef3dc012e1cc9ace634160d6d03b6dfa462
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-24 12:48:12 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,8 @@
+public class Sum{
+  // prepare a function to add and return all elements in ArrayList
+  public static void main(String[]args){
+  // Create ArrayList Object
+    // Add Elements to it using For Loop
+     System.out.println("Sum of ArrayList elements: " + total(arrayList));
+  // total is your function name it should return total value
+  }}
