543b303629c2268b412ac024d019287fb7bbb64f
KOushikguptha <koushikguptha2003@gmail.com>
2024-07-25 12:21:10 +0530

    LogicalLists

--- Sum.java
+++ Sum.java
@@ -1,8 +1,25 @@
-public class Sum{
-  // prepare a function to add and return all elements in ArrayList
-  public static void main(String[]args){
-  // Create ArrayList Object
-    // Add Elements to it using For Loop
-     System.out.println("Sum of ArrayList elements: " + total(arrayList));
-  // total is your function name it should return total value
-  }}
+import java.util.ArrayList;
+
+public class Sum {
+    // Method to add and return the sum of all elements in an ArrayList
+    public static int total(ArrayList<Integer> list) {
+        int sum = 0;
+        for (int item : list) {
+            sum += item;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        // Create an ArrayList object
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        // Load numbers into the ArrayList using a for loop
+        for (int i = 1; i <= 10; i++) {
+            arrayList.add(i);
+        }
+
+        // Print the sum of all elements in the ArrayList
+        System.out.println("Sum of ArrayList elements: " + total(arrayList));
+    }
+}
