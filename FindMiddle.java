6be3f3293cb40adac47e7dcc32dcc8ce66d99d78
gsbhavani <suryagogadi02@gmail.com>
2024-07-25 12:58:38 +0530

    LogicalList

--- FindMiddle.java
+++ FindMiddle.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class FindMiddle{
     public static <T> T Middle(ArrayList<T> list) {
         return list.get(list.size() / 2);
@@ -5,9 +7,13 @@ public class FindMiddle{
 // prepare a function to return middle value of LinkedList
   public static void main(String[]args){
 // create a LinkedList and ArrayList Objects
+ArrayList<Integer> arrayList = new ArrayList<Integer>();
     // prepare a for Loop of 1 to 10
+    for(int i = 1; i <= 10; i++){
     // and add elements of for loop in both Objects using add functions
-        System.out.println("Middle of ArrayList: " + Middle(arrayList));
+        arrayList.add(i);
+    }
     // write a print statement to print middle no of Linkedlist Object
+    System.out.println("Middle of ArrayList: " + Middle(arrayList));
 }
 }
--- Occurence.java
+++ Occurence.java
@@ -1,13 +1,34 @@
+import java.util.ArrayList;
 public class Occurence{
 // prepare a function to find out the occurence of a number
+public static <T> int countOccurrences(ArrayList<T> list, T element) {
+
+  int count = 0;
+
+  for (T item : list) {
+    if (item.equals(element)) {
+      count++;
+    }
+  }
+  return count;
+
+
+}
   // paramaters must contain Arraylist and a element to check it's occurence or repetition
   public static void main(String[]args){
    ArrayList<Integer> arrayList = new ArrayList<>();
      for (int i = 1; i <= 10; i++) {
             arrayList.add(i);
         }
-        arrayList.add(5);
-     int element = 5;
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(2);
+        arrayList.add(4);
+     int element = 2;
+     int element1=1;
+     int element2=4;
+        System.out.println("Occurrences of " + element1 + " in ArrayList: " + countOccurrences(arrayList, element1));
+        System.out.println("Occurrences of " + element2 + " in ArrayList: " + countOccurrences(arrayList, element2));
         System.out.println("Occurrences of " + element + " in ArrayList: " + countOccurrences(arrayList, element));
   }
 }
