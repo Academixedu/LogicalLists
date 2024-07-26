477c2d6fcfebc458cfa1dc47ecbf9903e259eab8
siva-245711 <sivanagendra809@gmail.com>
2024-07-26 19:14:57 +0530

    logical list

--- FindNum.java
+++ FindNum.java
@@ -1,10 +1,22 @@
-public class FindNum{
-// Write a method to find whether a particular element is present in an ArrayList
-public static void main(String[]args){
-// Create an Object for ArrayList
-  // Take a For Loop and Load Numbers in to ArrayList using add()
-  // Contains is the Method name which you will create
-System.out.println("ArrayList contains 5: " + contains(arrayList, 5));
-  // it should return either true or false
+import java.util.ArrayList;
+
+public class FindNum {
+    // Method to check if an element is present in the ArrayList
+    public static boolean contains(ArrayList<Integer> list, int num) {
+        return list.contains(num);
+    }
+
+    public static void main(String[] args) {
+        // Create an Object for ArrayList
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        // Load Numbers into ArrayList using add()
+        for (int i = 1; i <= 10; i++) {
+            arrayList.add(i);
+        }
+
+        // Check if the ArrayList contains a particular element
+        System.out.println("ArrayList contains 5: " + contains(arrayList, 5));
+    }
 }
-}
+
