235419079e2b5f596ca4509a9754a7732fa72f77
swapna-321 <swapna@gmail.com>
2024-07-27 23:00:43 +0530

    logical

--- Occurence.java
+++ Occurence.java
@@ -1,8 +1,19 @@
+import java.util.ArrayList;
 public class Occurence{
 // prepare a function to find out the occurence of a number
+  public static int countOccurrences(ArrayList<Integer> list, int element) {
+    int count = 0;
+    for (int i = 0; i < list.size(); i++) {
+      if (list.get(i) == element) {
+        count++;
+      }
+    }
+    return count;
+  } 
   // paramaters must contain Arraylist and a element to check it's occurence or repetition
+
   public static void main(String[]args){
-   ArrayList<Integer> arrayList = new ArrayList<>();
+   ArrayList<Integer> arrayList = new ArrayList<Integer>();
      for (int i = 1; i <= 10; i++) {
             arrayList.add(i);
         }
