4619606ff6412570be2e7993f14aaa7402903342
vijay767492 <vijaydandu59@gmail.com>
2024-08-28 17:49:46 +0530

    vijay dandu

--- FindNum.java
+++ FindNum.java
@@ -1,10 +1,20 @@
-public class FindNum{
-// Write a method to find whether a particular element is present in an ArrayList
-public static void main(String[]args){
-// Create an Object for ArrayList
-  // Take a For Loop and Load Numbers in to ArrayList using add()
-  // Contains is the Method name which you will create
-System.out.println("ArrayList contains 5: " + contains(arrayList, 5));
-  // it should return either true or false
+import java.util.ArrayList;
+
+public class FindNum {
+
+    public static <T> boolean contains(ArrayList<T> list, T element) {
+        return list.contains(element);
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        for (int i = 1; i <= 10; i++) {
+            arrayList.add(i);
+        }
+
+        System.out.println("ArrayList contains 5: " + contains(arrayList, 5));
+        System.out.println("ArrayList contains 15: " + contains(arrayList, 15));
+    }
 }
-}
+
--- Sum.java
+++ Sum.java
@@ -1,8 +1,23 @@
-public class Sum{
-  // prepare a function to add and return all elements in ArrayList
-  public static void main(String[]args){
-  // Create ArrayList Object
-    // Add Elements to it using For Loop
-     System.out.println("Sum of ArrayList elements: " + total(arrayList));
-  // total is your function name it should return total value
-  }}
+import java.util.ArrayList;
+
+public class Sum {
+
+    // Function to calculate the sum of all elements in an ArrayList
+    public static int total(ArrayList<Integer> list) {
+        int sum = 0;
+        for (int element : list) {
+            sum += element;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        for (int i = 1; i <= 10; i++) {
+            arrayList.add(i);
+        }
+
+        System.out.println("Sum of ArrayList elements: " + total(arrayList));
+    }
+}
