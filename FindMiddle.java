d5cf2814bf9da04bae0ce199c54aa33d7ef8d2d6
Talari-Lavanya <lavanya62811@gmail.com>
2024-08-28 18:48:30 +0530

    Lavanya

--- FindMiddle.java
+++ FindMiddle.java
@@ -1,13 +1,26 @@
+import java.util.*;
 public class FindMiddle{
-    public static <T> T Middle(ArrayList<T> list) {
+    public static <T> T Middle(ArrayList<T> list){
         return list.get(list.size() / 2);
     }
-// prepare a function to return middle value of LinkedList
+    // prepare a function to return middle value of LinkedList
+    public static <T> T Middle(LinkedList<T> list){
+    
+        return list.get(list.size() / 3);
+    }
+
   public static void main(String[]args){
 // create a LinkedList and ArrayList Objects
+    ArrayList <Integer> arrayList = new ArrayList <Integer>();
+    LinkedList <Integer> linkedList = new LinkedList <Integer>();
     // prepare a for Loop of 1 to 10
     // and add elements of for loop in both Objects using add functions
+    for(int i=1;i<=10;i++){
+        arrayList.add(i);
+        linkedList.add(i);
+    }
         System.out.println("Middle of ArrayList: " + Middle(arrayList));
     // write a print statement to print middle no of Linkedlist Object
+    System.out.println("Middle of LinkedList: " + Middle(linkedList));
 }
 }
--- Occurence.java
+++ Occurence.java
@@ -1,5 +1,15 @@
+import java.util.*;
 public class Occurence{
 // prepare a function to find out the occurence of a number
+public static int countOccurance(ArrayList<Integer> arrayList, int element){
+  int count=0;
+  for(int num : arrayList){
+    if(num==element){
+      count++;
+    }
+  }
+  return count;
+}
   // paramaters must contain Arraylist and a element to check it's occurence or repetition
   public static void main(String[]args){
    ArrayList<Integer> arrayList = new ArrayList<>();
@@ -8,6 +18,6 @@ public class Occurence{
         }
         arrayList.add(5);
      int element = 5;
-        System.out.println("Occurrences of " + element + " in ArrayList: " + countOccurrences(arrayList, element));
+        System.out.println("Occurrences of " + element + " in ArrayList: " + countOccurance(arrayList, element));
   }
 }
