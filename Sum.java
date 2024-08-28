6b1c94781faf9635c3401dac73b00292fce20f3f
karthhiik <karthikthatikonda18@gmail.com>
2024-08-28 20:05:48 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -1,8 +1,24 @@
-public class Sum{
-  // prepare a function to add and return all elements in ArrayList
-  public static void main(String[]args){
-  // Create ArrayList Object
-    // Add Elements to it using For Loop
-     System.out.println("Sum of ArrayList elements: " + total(arrayList));
-  // total is your function name it should return total value
-  }}
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sum {
+  
+  public int total(List<Integer> list) {
+    int sum = 0;
+    for (int i : list) {
+      sum += i;
+    }
+    return sum;
+  }
+  
+  public static void main(String[] args) {
+    List<Integer> arrayList = new ArrayList<>();
+    
+    for (int i = 0; i < 7; i++) {
+      arrayList.add(i + 1);
+    }
+    
+    Sum sm = new Sum();
+    System.out.println("Sum of ArrayList elements: " + sm.total(arrayList));
+  }
+}
