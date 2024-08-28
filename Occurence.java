8cda20649a760d5bee5dc7f0937fed984e50ea77
karthhiik <karthikthatikonda18@gmail.com>
2024-08-28 20:11:39 +0530

    Update Occurence.java

--- Occurence.java
+++ Occurence.java
@@ -1,13 +1,25 @@
-public class Occurence{
-// prepare a function to find out the occurence of a number
-  // paramaters must contain Arraylist and a element to check it's occurence or repetition
-  public static void main(String[]args){
-   ArrayList<Integer> arrayList = new ArrayList<>();
-     for (int i = 1; i <= 10; i++) {
+import java.util.*;
+
+public class Occurrence {
+
+    // Method to count occurrences of a specific element in the ArrayList
+    public static int countOccurrences(ArrayList<Integer> list, int element) {
+        int count = 0;
+        for (int i : list) {
+            if (i == element) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+                for (int i = 1; i <= 10; i++) {
             arrayList.add(i);
         }
-        arrayList.add(5);
-     int element = 5;
-        System.out.println("Occurrences of " + element + " in ArrayList: " + countOccurrences(arrayList, element));
-  }
+        arrayList.add(9);
+        int element = 9;
+                System.out.println("Occurrences of " + element + " in ArrayList: " + countOccurrences(arrayList, element));
+    }
 }
