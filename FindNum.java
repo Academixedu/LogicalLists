e98d9f5e675e27bc296afc78b402e56d8937ab4d
Arbaz-01 <arbazshaibu@gmail.com>
2024-08-29 11:12:20 +0530

    aaz

--- FindNum.java
+++ FindNum.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 
 public class FindNum{
+
+
+
   public static boolean containss(ArrayList<Integer>arrayList,int q){
     for (Integer b : arrayList) {
       if (b==q) {
@@ -11,20 +14,18 @@ public class FindNum{
     }
     
 
-// Write a method to find whether a particular element is present in an ArrayList
 public static void main(String[]args){
-  ArrayList<Integer>e=new ArrayList<Integer>();
-     e.add(2);
-     e.add(5);
-   for (Integer a : e) {
+  ArrayList<Integer>e=new ArrayList<>();
     
-    System.out.println(a);
+   for (int i=1;i<=8;i++) {
+    e.add(i);
+    System.out.println(i);
    }
-FindNum op=new FindNum();
-  boolean contains=FindNum.containss(e, 5);
-  // Contains is the Method name which you will create
+
  
-System.out.println("ArrayList contains 5: " + containss(arrayList, 5));
-  // it should return either true or false
+
+
+System.out.println("ArrayList contains 8: " + containss( e, 11));
+ 
 }
 }
--- Occurence.java
+++ Occurence.java
@@ -13,8 +13,6 @@ public class Occurence{
     
   }
 
-// prepare a function to find out the occurence of a number
-  // paramaters must contain Arraylist and a element to check it's occurence or repetition
   public static void main(String[]args){
    ArrayList<Integer> arrayList = new ArrayList<>();
      for (int i = 1; i <= 10; i++) {
@@ -22,9 +20,7 @@ public class Occurence{
         }
         arrayList.add(5);
       int element = 5;
-        arrayList.add(countOccurrences(arrayList,element));
-      Occurence man = new Occurence();
-        int occurrences = man.countOccurrences(arrayList, element);
+        
         
 
         System.out.println("Occurrences of " + element + " in ArrayList: " + countOccurrences(arrayList, element));
